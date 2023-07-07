c82c01ef4fb98db77cb364c84a13a00828bf98c2
PharisMods <rmassey3234@gmail.com>
2023-07-07 01:24:11 -0500

    Nest docs more in packager to avoid MO2 showing conflicts between my mods

--- packager.bat
+++ packager.bat
@@ -1,3 +1,3 @@
 7z a PharisMagickaRegeneration.7z Scripts PharisMagickaRegen-GameSettings.esp PharisMagickaRegeneration.omwscripts changelog.md README.md
-7z rn PharisMagickaRegeneration.7z changelog.md Docs/changelog.md
-7z rn PharisMagickaRegeneration.7z README.md Docs/README.md
+7z rn PharisMagickaRegeneration.7z changelog.md Docs/Pharis/PharisMagickaRegeneration/changelog.md
+7z rn PharisMagickaRegeneration.7z README.md Docs/Pharis/PharisMagickaRegeneration/README.md
